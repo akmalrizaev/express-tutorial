0ee457d345aa7e512adfabf4eb70fe8c81e8d9e2
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-25 14:06:31 +0500

    HTTP Request Object

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' });
-  res.write('<h1>Home Page </h1>');
-  res.end();
+  //   console.log(req.method);
+  //   console.log(req.url);
+
+  const url = req.url;
+
+  //  home page
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Home Page </h1>');
+    res.end();
+  }
+  //   about page
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>About Page </h1>');
+    res.end();
+  }
+  //   404
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>Page Not Found </h1>');
+    res.end();
+  }
 });
 
 server.listen(5000);
