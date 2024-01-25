55db6601e7c9cd9e97923e391f7bdfec1815febf
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-25 17:23:03 +0500

    HTTP - HTML File

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const http = require('http');
+const { readFileSync } = require('fs');
+
+// get all files
+const homePage = readFileSync('./index.html');
 
 const server = http.createServer((req, res) => {
   //   console.log(req.method);
@@ -9,7 +13,7 @@ const server = http.createServer((req, res) => {
   //  home page
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
-    res.write('<h1>Home Page </h1>');
+    res.write(homePage);
     res.end();
   }
   //   about page
