033bef0cd59c52e155fba2080202a14e3de898b4
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-26 09:09:32 +0500

    JSON - Basics

--- app.js
+++ app.js
@@ -1,32 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const { products } = require('./data');
 
-// setup static and middleware
-app.use(express.static('./public'));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-// adding to static assets
-// SSR
-// });
-
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page');
-});
-
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>Resource Not Found</h1>');
+app.get('/', (req, res) => {
+  res.json(products);
 });
 
 app.listen(5000, () => {
   console.log('server is listening on port 5000...');
 });
-
-// app.get
-// app.post
-//app.put
-// app.delete
-// app.all
-//app.use
-// app.listen
