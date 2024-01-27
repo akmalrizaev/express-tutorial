2de2f4e3b400c3450cbd1ad1980ce7df15c218e8
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-27 13:18:43 +0500

    Methods - POST (Javascript Example)

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.static('./methods-public'));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 
+// parse json
+app.use(express.json());
+
 // HTTP Methods
 // GET - Read Data
 // POST - Insert Data
@@ -16,6 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post('/login', (req, res) => {
